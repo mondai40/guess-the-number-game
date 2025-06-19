275c21dd671428f464118a934e38edd89b5df5eb
monotsuku20 <monotsuku20@Ubuntu20.myguest.virtualbox.org>
2025-06-19 17:20:01 +0900

    add number judge logic

--- game.py
+++ game.py
@@ -5,6 +5,7 @@
 # ユーザーが数字を推測するための試行回数を制限する
 
 import sys
+import random
 
 def main():
     try:
@@ -22,6 +23,7 @@ def main():
 """
         )
 
+        playStart = ""
         while True:
             playStart = input("ゲームをプレイしますか？y = yes、n = noをタイプしてください。")
             #  入力値がy, yes, n, noのみ許可
@@ -33,6 +35,52 @@ def main():
         if playStart in ["n", "no"]:
             raise EOFError()
         
+        print("ゲームスタート")
+        min_number = None
+        while True:
+            try:
+                min_number = int(input("最小値を入力してください。"))
+                break
+            except ValueError:
+                print("入力値が間違えています。数値を入力してください。")
+        
+        max_number = None
+        while True:
+            try:
+                max_number = int(input("最大値を入力してください。"))
+            except ValueError:
+                print("入力値が間違えています。数値を入力してください。")
+                continue
+
+            if max_number < min_number:
+                print("入力値が間違えています。最大値は最小値以上の数値にしてください。")
+                continue
+
+            break
+
+        print(min_number)
+        print(max_number)
+
+        random_number = random.randint(min_number, max_number)
+
+        try_count = 1
+        while try_count <= 5:
+            try:
+                answer = int(input(str(try_count) + "回目の回答: 数字を予想してください。"))
+                if answer > random_number:
+                    print("Your answer is greater than the random number.")
+                elif answer < random_number:
+                    print("Your answer is smaller than the random number.")
+                else:
+                    print("You win!")
+                    break
+
+            except ValueError:
+                print("入力値が間違えています。数値を入力してください。")
+            
+            try_count += 1
+
+        print("You lose! 正解は" + str(random_number) + "でした" if try_count >= 5 else "a") 
             
     except KeyboardInterrupt:
         print("\n\nゲームを中断しました。")
